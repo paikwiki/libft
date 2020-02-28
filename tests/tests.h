01cab137997fdccfd0aa095e48ea1f94a7dcf1df
cbaek <cbaek@student.42seoul.kr>
2020-02-28 12:50:37 +0900

    Add library on tests.h: ctype.h

--- tests/tests.h
+++ tests/tests.h
@@ -6,7 +6,7 @@
 /*   By: cbaek <cbaek@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/27 22:02:20 by cbaek             #+#    #+#             */
-/*   Updated: 2020/02/28 11:04:13 by cbaek            ###   ########.fr       */
+/*   Updated: 2020/02/28 12:46:52 by cbaek            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,11 @@
 #include <stdio.h>
 #include <assert.h>
 #include <stdbool.h>
+#include <ctype.h>
 #include "../libft.h"
 
 void test_ft_isdigit();
 void test_ft_isalpha();
 void test_ft_isascii();
+void test_ft_isalnum();
 #endif
