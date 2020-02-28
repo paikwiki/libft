27a4c90031a22cf649f1472c1ecb8ee967fd50a9
cbaek <cbaek@student.42seoul.kr>
2020-02-28 12:50:57 +0900

    Done ft_isalnum

---
+++ tests/test_ft_isalnum.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test_ft_isalnum.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cbaek <cbaek@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/28 12:39:52 by cbaek             #+#    #+#             */
+/*   Updated: 2020/02/28 12:46:23 by cbaek            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "tests.h"
+
+void test_ft_isalnum()
+{
+	int i;
+
+	for (i = -10; i <= 137; i++)
+	{
+		if (isdigit(i) || isalpha(i))
+			assert(ft_isalnum(i) != 0 && "test_ft_isalnum()");
+		else
+			assert(ft_isalnum(i) == 0 && "test_ftisalnum()");
+	}
+}
