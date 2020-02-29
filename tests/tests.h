a7db2b79de159598759480e1da4f87804cafd6cc
cbaek <cbaek@student.42seoul.kr>
2020-02-29 11:51:41 +0900

    Reorder prototypes of all test function

--- tests/tests.h
+++ tests/tests.h
@@ -6,7 +6,7 @@
 /*   By: cbaek <cbaek@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/27 22:02:20 by cbaek             #+#    #+#             */
-/*   Updated: 2020/02/29 11:49:23 by cbaek            ###   ########.fr       */
+/*   Updated: 2020/02/29 11:51:17 by cbaek            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,16 +22,6 @@
 #include <string.h>
 #include "../libft.h"
 
-void test_ft_isdigit();
-void test_ft_isalpha();
-void test_ft_isascii();
-void test_ft_isalnum();
-void test_ft_isprint();
-void test_ft_strlen();
-void test_ft_toupper();
-void test_ft_tolower();
-void test_ft_strchr();
-void test_ft_atoi();
 void test_ft_memset();
 void test_ft_bzero();
 void test_ft_memcpy();
@@ -39,9 +29,19 @@ void test_ft_memccpy();
 void test_ft_memmove();
 void test_ft_memchr();
 void test_ft_memcmp();
+void test_ft_strlen();
 void test_ft_strlcpy();
 void test_ft_strlcat();
+void test_ft_strchr();
 void test_ft_strrchr();
 void test_ft_strnstr();
 void test_ft_strncmp();
+void test_ft_atoi();
+void test_ft_isdigit();
+void test_ft_isalpha();
+void test_ft_isascii();
+void test_ft_isalnum();
+void test_ft_isprint();
+void test_ft_toupper();
+void test_ft_tolower();
 #endif
