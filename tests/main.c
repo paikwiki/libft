c33551852b4860c5f7a102d8a7c0ca106213e493
cbaek <cbaek@student.42seoul.kr>
2020-02-29 11:56:08 +0900

    Add all test function with comment if it needs to

--- tests/main.c
+++ tests/main.c
@@ -6,7 +6,7 @@
 /*   By: cbaek <cbaek@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/27 22:14:38 by cbaek             #+#    #+#             */
-/*   Updated: 2020/02/29 10:54:08 by cbaek            ###   ########.fr       */
+/*   Updated: 2020/02/29 11:54:27 by cbaek            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,27 @@
 
 int main(void)
 {
+	// test_ft_memset();
+	// test_ft_bzero();
+	// test_ft_memcpy();
+	// test_ft_memccpy();
+	// test_ft_memmove();
+	// test_ft_memchr();
+	// test_ft_memcmp();
+	test_ft_strlen();
+	// test_ft_strlcpy();
+	// test_ft_strlcat();
+	test_ft_strchr();
+	// test_ft_strrchr();
+	// test_ft_strnstr();
+	// test_ft_strncmp();
+	test_ft_atoi();
 	test_ft_isdigit();
 	test_ft_isalpha();
 	test_ft_isascii();
 	test_ft_isalnum();
 	test_ft_isprint();
-	test_ft_strlen();
 	test_ft_toupper();
 	test_ft_tolower();
-	test_ft_strchr();
-	test_ft_atoi();
 	return (0);
 }
