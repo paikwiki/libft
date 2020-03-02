a4dd6cfa61a7f1d19d131d533256bf7cbba1856d
cbaek <cbaek@student.42seoul.kr>
2020-03-03 01:17:58 +0900

    Debug ft_strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: cbaek <cbaek@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/01 22:40:38 by cbaek             #+#    #+#             */
-/*   Updated: 2020/03/01 23:23:26 by cbaek            ###   ########.fr       */
+/*   Updated: 2020/03/03 01:07:00 by cbaek            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,11 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	i = 0;
 	if (n == 0)
 		return (0);
-	if (s2[0] == 0)
+	if (s1[0] == 0 || s2[0] == 0)
 		return ((unsigned char)s1[0] - (unsigned char)s2[0]);
-	while (s1[i] != 0 && n > 0)
+	while (n > 0)
 	{
-		if (s2[i] == 0)
+		if (s1[i] == 0 || s2[i] == 0)
 		{
 			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 		}
