382c1f1434163bf89599c8b6bbcfed0f80f5ce81
cbaek <cbaek@student.42seoul.kr>
2020-03-03 19:17:50 +0900

    Done 4 functions
    - ft_bzero()
    - ft_memccpy()
    - ft_memcpy()
    - ft_memset()

---
+++ ft_memccpy.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memccpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cbaek <cbaek@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/03/03 16:46:23 by cbaek             #+#    #+#             */
+/*   Updated: 2020/03/03 17:11:01 by cbaek            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
+{
+	size_t	i;
+
+	if (dst == 0 || src == 0)
+		return (0);
+	i = 0;
+	while (i < n)
+	{
+		((unsigned char *)dst)[i] = ((unsigned char *)src)[i];
+		if (((unsigned char *)dst)[i] == (unsigned char)c)
+			return (dst + i + 1);
+		i++;
+	}
+	return (0);
+}
---
+++ ft_memcpy.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cbaek <cbaek@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/03/03 12:07:40 by cbaek             #+#    #+#             */
+/*   Updated: 2020/03/03 15:37:27 by cbaek            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void *ft_memcpy(void *dst, const void *src, size_t n)
+{
+	size_t i;
+
+	if (dst == 0 || src == 0)
+		return (0);
+	i = 0;
+	while (i < n)
+	{
+		((unsigned char *)dst)[i] = ((unsigned char *)src)[i];
+		i++;
+	}
+	return (dst);
+}
