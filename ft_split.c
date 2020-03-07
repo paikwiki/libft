cbc056d72d1586ebf7754e47975dc5be3383f3a3
cbaek <cbaek@student.42seoul.kr>
2020-03-07 21:11:10 +0900

    Edit 42 header: change name and email

--- ft_split.c
+++ ft_split.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: chPaik <chPaik@student.42.fr>              +#+  +:+       +#+        */
+/*   By: cbaek <cbaek@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/06 14:28:53 by cbaek             #+#    #+#             */
-/*   Updated: 2020/03/06 22:28:17 by chPaik           ###   ########.fr       */
+/*   Updated: 2020/03/07 21:09:34 by cbaek            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,7 +42,7 @@ static void	setstrs_str(char **strs, char *s, char c, size_t i)
 {
 	int	cnt;
 	int	strlen;
-	
+
 	cnt = 0;
 	strlen = 0;
 	while(s[i] == c)
@@ -60,7 +60,7 @@ static void	setstrs_str(char **strs, char *s, char c, size_t i)
 		{
 			strs[cnt][strlen] = s[i];
 			strs[cnt][strlen + 1] = 0;
-		}	
+		}
 		i++;
 	}
 }
