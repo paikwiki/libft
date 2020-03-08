a56d18172d26c2e6e4a680cce8e4359c1b995208
cbaek <cbaek@student.42seoul.kr>
2020-03-08 17:48:28 +0900

    Done 3 functions and finish Part 2

---
+++ ft_strmapi.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cbaek <cbaek@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/03/07 23:14:23 by cbaek             #+#    #+#             */
+/*   Updated: 2020/03/07 23:22:21 by cbaek            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char	*str;
+	size_t	len;
+	size_t	i;
+
+	if (!s)
+		return 0;
+
+	len = 0;
+	while (s[len] != 0)
+		len++;
+	str = (char *)malloc(sizeof(char *) * (len + 1));
+	i = 0;
+	while (i < len)
+	{
+		str[i] = (*f)(i, s[i]);
+		i++;
+	}
+	str[i] = 0;
+	return (str);
+}
