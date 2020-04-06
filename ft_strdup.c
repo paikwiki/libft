7170194dd3081258392a1af8321b41a0c939e864
cbaek <cbaek@student.42seoul.kr>
2020-04-06 18:38:20 +0900

    Fix ft_strdup: init variable '*duplicate' with null

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: cbaek <cbaek@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/05 12:12:12 by cbaek             #+#    #+#             */
-/*   Updated: 2020/03/05 12:33:16 by cbaek            ###   ########.fr       */
+/*   Updated: 2020/04/06 18:36:29 by cbaek            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,24 @@
 char	*ft_strdup(const char *s1)
 {
 	char	*duplicate;
-	int		i;
+	int		idx;
+	int		d_idx;
 
-	i = 0;
-	while (s1[i] != 0)
-		i++;
-	if ((duplicate = (char *)malloc(sizeof(char) * i)) == 0)
+	idx = 0;
+	while (s1[idx] != 0)
+		idx++;
+	if ((duplicate = (char *)malloc(sizeof(char) * (idx + 1))) == 0)
 		return (0);
-	i = 0;
-	while (s1[i] != 0)
+	d_idx = 0;
+	while (d_idx <= idx)
 	{
-		duplicate[i] = s1[i];
-		i++;
+		duplicate[d_idx++] = 0;
 	}
-	return (&duplicate[0]);
+	idx = 0;
+	while (s1[idx] != 0)
+	{
+		duplicate[idx] = s1[idx];
+		idx++;
+	}
+	return (duplicate);
 }
