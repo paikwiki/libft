18ee4c67dbccc7f1ecdb828e0aa53aae9e1d2e28
cbaek <cbaek@student.42seoul.kr>
2020-04-07 10:19:23 +0900

    Edit ft_lstlast()

--- ft_lstlast.c
+++ ft_lstlast.c
@@ -6,7 +6,7 @@
 /*   By: cbaek <cbaek@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/08 00:05:17 by cbaek             #+#    #+#             */
-/*   Updated: 2020/04/06 15:16:34 by cbaek            ###   ########.fr       */
+/*   Updated: 2020/04/06 22:00:52 by cbaek            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,18 @@
 
 t_list	*ft_lstlast(t_list *lst)
 {
-	t_list *temp;
+	t_list *result;
 
-	temp = lst;
-	while (temp->next != 0)
-		temp = temp->next;
-	return (temp);
+	if (!lst)
+		return (0);
+	result = lst;
+	if (result->next == 0)
+		return (result);
+	while (1)
+	{
+		if (result->next == 0)
+			return (result);
+		result = result->next;
+	}
+	return (0);
 }
