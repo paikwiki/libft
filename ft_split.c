3295eee81a0eea0ef58fde40b4761d97ed6025fd
cbaek <cbaek@student.42seoul.kr>
2020-04-15 22:21:14 +0900

    Fix ft_split()

--- ft_split.c
+++ ft_split.c
@@ -6,38 +6,35 @@
 /*   By: cbaek <cbaek@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/06 14:28:53 by cbaek             #+#    #+#             */
-/*   Updated: 2020/04/13 14:00:01 by cbaek            ###   ########.fr       */
+/*   Updated: 2020/04/15 21:58:42 by cbaek            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static void	setstrs_mem(char **strs, char *s, char c)
+static void	setstrs_mem(char **strs, char *s, char c, size_t idx)
 {
-	int i;
 	int cnt;
 	int strlen;
 
-	i = 0;
 	cnt = 0;
 	strlen = 0;
-	if (!s)
-		return ;
-	while (s[i] == c)
-		i++;
-	while (s[i] != 0)
+	while (s[idx] == c)
+		idx++;
+	while (s[idx] != 0)
 	{
-		if ((s[i - 1] != c && s[i] == c) || (s[i + 1] == 0 && s[i] != c))
+		if ((idx != 0 && (s[idx - 1] != c && s[idx] == c))
+				|| (s[idx + 1] == 0 && s[idx] != c))
 		{
-			if (s[i + 1] == 0 && s[i] != c)
+			if (s[idx + 1] == 0 && s[idx] != c)
 				strlen++;
 			if (!(strs[cnt++] = (char *)malloc(sizeof(char) * (strlen + 1))))
 				return ;
 			strlen = 0;
 		}
-		else if (s[i] != c)
+		else if (s[idx] != c)
 			strlen++;
-		i++;
+		idx++;
 	}
 }
 
@@ -48,15 +45,13 @@ static void	setstrs_str(char **strs, char *s, char c, size_t idx)
 
 	cnt = 0;
 	strlen = 0;
-	if (!s)
-		return ;
 	while (s[idx] == c)
 		idx++;
 	while (s[idx] != 0)
 	{
 		if ((s[idx] != c) && (s[idx + 1] != 0))
 			strs[cnt][strlen++] = s[idx];
-		else if (s[idx - 1] != c && s[idx] == c)
+		else if ((idx != 0 && s[idx - 1] != c) && s[idx] == c)
 		{
 			strs[cnt++][strlen] = 0;
 			strlen = 0;
@@ -73,25 +68,26 @@ static void	setstrs_str(char **strs, char *s, char c, size_t idx)
 char		**ft_split(char const *s, char c)
 {
 	char	**strs;
-	size_t	i;
+	size_t	idx;
 	size_t	cnt;
 
-	if (!s || ((!s && !*s) && !c))
+	if (!s)
 		return (0);
-	i = 0;
+	idx = 0;
 	cnt = 0;
-	while (s[i] == c)
-		i++;
-	while (s[i] != 0)
+	while (s[idx] == c)
+		idx++;
+	while (s[idx] != 0)
 	{
-		if ((s[i - 1] != c && s[i] == c) || (s[i + 1] == 0 && s[i] != c))
+		if (((idx != 0 && s[idx - 1] != c) && s[idx] == c) ||
+				(s[idx + 1] == 0 && s[idx] != c))
 			cnt++;
-		i++;
+		idx++;
 	}
 	if (!(strs = (char **)malloc(sizeof(char *) * (cnt + 1))))
 		return (0);
 	strs[cnt] = 0;
-	setstrs_mem(strs, (char *)s, c);
+	setstrs_mem(strs, (char *)s, c, 0);
 	setstrs_str(strs, (char *)s, c, 0);
 	return (strs);
 }
