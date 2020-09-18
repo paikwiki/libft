8af56f6c38ceebaa78b97a9e2994281616c149cf
paikwiki <paikwiki@gmail.com>
2020-09-18 20:32:49 +0900

    Improve ft_strjoin()
    - remove a var "str_len": reuse "len"
    - replace "0" to "\0" when it is the end of a string

--- codes/ft_strjoin.c
+++ codes/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: cbaek <cbaek@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/06 10:00:11 by cbaek             #+#    #+#             */
-/*   Updated: 2020/09/02 19:23:16 by cbaek            ###   ########.fr       */
+/*   Updated: 2020/09/18 20:32:11 by cbaek            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,24 +16,23 @@ char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*str;
 	size_t	len;
-	size_t	str_len;
 
 	if (!s1 || !s2)
 		return (0);
-	str_len = ft_strlen(s1) + ft_strlen(s2);
-	if (str_len == 0)
+	len = ft_strlen(s1) + ft_strlen(s2);
+	if (len == 0)
 	{
 		str = (char *)malloc(sizeof(char));
-		str[0] = 0;
+		str[0] = '\0';
 		return (str);
 	}
-	if ((str = (char *)malloc(sizeof(char) * (str_len + 1))) == 0)
+	if ((str = (char *)malloc(sizeof(char) * (len + 1))) == 0)
 		return (0);
 	len = 0;
 	while (*s1 != 0)
 		str[len++] = *s1++;
 	while (*s2 != 0)
 		str[len++] = *s2++;
-	str[len] = 0;
+	str[len] = '\0';
 	return (str);
 }
